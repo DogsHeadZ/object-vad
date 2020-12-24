bb67628ce14ec4903c6efe4721fc428ca0dfcf33
feihu <565807094@qq.com>
2020-12-24 16:11:52 +0800

    test new branch zwh

--- testgrammar.py
+++ testgrammar.py
@@ -20,8 +20,23 @@ import argparse
 # model = convAE(train_dataset_args['c'], train_dataset_args['t_length'], **config['model_args'])
 
 
+# import sys
+# sys.path.insert(0, './yolov5')
+#
+# anomaly_score_total_list = []
+# anomaly_score_total_list += [1,2]
+# anomaly_score_total_list += [4,5]
+# print(anomaly_score_total_list)
+#
+# print(torch.load('yolov5s.pt'))
 
-anomaly_score_total_list = []
-anomaly_score_total_list += [1,2]
-anomaly_score_total_list += [4,5]
-print(anomaly_score_total_list)
+import cv2
+
+img=cv2.imread('1.jpg')
+
+#(5, 5)表示高斯矩阵的长与宽都是5，标准差取0
+blur=cv2.GaussianBlur(img,(5,5),0)
+
+# cv2.imshow('GaussianBlur',blur)
+cv2.imwrite('blur.jpg', blur)
+# cv2.destroyAllWindows()
