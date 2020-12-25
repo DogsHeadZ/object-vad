d320e6a6801a4414a608d4e14e0dee1310c8737d
lyx3911 <913797866@qq.com>
2020-12-25 19:15:36 +0800

    add object detect with motion

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from tensorboardX import SummaryWriter
 from torch.autograd import Variable
 
 
-import utils
+import Utils
 from vad_dataloader import VadDataset
 from model.preAE import PreAE
 
@@ -36,8 +36,8 @@ def train(config):
     if args.tag is not None:
         svname += '_' + args.tag
     save_path = os.path.join('./save', svname)
-    utils.set_save_path(save_path)
-    utils.set_log_path(save_path)
+    Utils.set_save_path(save_path)
+    Utils.set_log_path(save_path)
     writer = SummaryWriter(os.path.join(save_path, 'tensorboard'))
     yaml.dump(config, open(os.path.join(save_path, 'classifier_config.yaml'), 'w'))
 
@@ -45,15 +45,18 @@ def train(config):
     # train
     train_folder = config['dataset_path'] + config['train_dataset_type'] + "/training/frames"
     test_folder = config['dataset_path'] + config['train_dataset_type'] + "/testing/frames"
+    train_bboxes = "./bboxes/" + config['train_dataset_type'] + "/train"
+    test_bboxes = "./bboxes/" + config['train_dataset_type'] + "/test"
+
 
     # Loading dataset
     train_dataset_args = config['train_dataset_args']
     test_dataset_args = config['test_dataset_args']
-    train_dataset = VadDataset(train_folder, transforms.Compose([
+    train_dataset = VadDataset(train_folder, train_bboxes, transforms.Compose([
         transforms.ToTensor(),
     ]), resize_height=train_dataset_args['h'], resize_width=train_dataset_args['w'], time_step=train_dataset_args['t_length'] - 1)
 
-    test_dataset = VadDataset(test_folder, transforms.Compose([
+    test_dataset = VadDataset(test_folder, test_bboxes, transforms.Compose([
         transforms.ToTensor(),
     ]), resize_height=test_dataset_args['h'], resize_width=test_dataset_args['w'], time_step=test_dataset_args['t_length'] - 1)
 
@@ -72,7 +75,8 @@ def train(config):
     label_length = 0
     psnr_list = {}
     for video in sorted(videos_list):
-        video_name = video.split('/')[-1]
+        # video_name = video.split('/')[-1]
+        video_name = os.path.split(video)[-1]
         videos[video_name] = {}
         videos[video_name]['path'] = video
         videos[video_name]['frame'] = glob.glob(os.path.join(video, '*.jpg'))
@@ -89,7 +93,7 @@ def train(config):
     params_encoder = list(model.encoder.parameters())
     params_decoder = list(model.decoder.parameters())
     params = params_encoder + params_decoder
-    optimizer, lr_scheduler = utils.make_optimizer(
+    optimizer, lr_scheduler = Utils.make_optimizer(
         params, config['optimizer'], config['optimizer_args'])
 
     loss_func_mse = nn.MSELoss(reduction='none')
@@ -101,7 +105,7 @@ def train(config):
         model = nn.DataParallel(model)
 
     # Training
-    utils.log('Start train')
+    Utils.log('Start train')
     max_accuracy = 0
     base_channel_num  = train_dataset_args['c'] * (train_dataset_args['t_length'] - 1)
     save_epoch = 5 if config['save_epoch'] is None else config['save_epoch']
@@ -118,40 +122,44 @@ def train(config):
             optimizer.step()
         lr_scheduler.step()
 
-        utils.log('----------------------------------------')
-        utils.log('Epoch:' + str(epoch + 1))
-        utils.log('----------------------------------------')
-        utils.log('Loss: Reconstruction {:.6f}'.format(loss_pixel.item()))
+        Utils.log('----------------------------------------')
+        Utils.log('Epoch:' + str(epoch + 1))
+        Utils.log('----------------------------------------')
+        Utils.log('Loss: Reconstruction {:.6f}'.format(loss_pixel.item()))
 
         # Testing
-        utils.log('Evaluation of ' + config['test_dataset_type'])
+        Utils.log('Evaluation of ' + config['test_dataset_type'])
         for video in sorted(videos_list):
-            video_name = video.split('/')[-1]
+            # video_name = video.split('/')[-1]
+            video_name = os.path.split(video)[-1]
             psnr_list[video_name] = []
 
         model.eval()
         video_num = 0
-        label_length += videos[videos_list[video_num].split('/')[-1]]['length']
+        # label_length += videos[videos_list[video_num].split('/')[-1]]['length']
+        label_length += videos[os.path.split(videos_list[video_num])[-1]]['length']
         for k, imgs in enumerate(tqdm(test_dataloader, desc='test', leave=False)):
             if k == label_length - 4 * (video_num + 1):
                 video_num += 1
-                label_length += videos[videos_list[video_num].split('/')[-1]]['length']
+                # label_length += videos[videos_list[video_num].split('/')[-1]]['length']
+                label_length += videos[os.path.split(videos_list[video_num])[-1]]['length']
             imgs = imgs.cuda()
             outputs, feas = model.forward(imgs[:, 0: base_channel_num])
             mse_imgs = torch.mean(loss_func_mse((outputs[0] + 1) / 2, (imgs[0, base_channel_num:] + 1) / 2)).item()
-            psnr_list[videos_list[video_num].split('/')[-1]].append(utils.psnr(mse_imgs))
+            psnr_list[videos_list[video_num].split('/')[-1]].append(Utils.psnr(mse_imgs))
 
         # Measuring the abnormality score and the AUC
         anomaly_score_total_list = []
         for video in sorted(videos_list):
-            video_name = video.split('/')[-1]
-            anomaly_score_total_list += utils.anomaly_score_list(psnr_list[video_name])
+            # video_name = video.split('/')[-1]
+            video_name = os.path.split(video)[-1]
+            anomaly_score_total_list += Utils.anomaly_score_list(psnr_list[video_name])
 
         anomaly_score_total_list = np.asarray(anomaly_score_total_list)
-        accuracy = utils.AUC(anomaly_score_total_list, np.expand_dims(1 - labels_list, 0))
+        accuracy = Utils.AUC(anomaly_score_total_list, np.expand_dims(1 - labels_list, 0))
 
-        utils.log('The result of ' + config['test_dataset_type'])
-        utils.log('AUC: ' + str(accuracy * 100) + '%')
+        Utils.log('The result of ' + config['test_dataset_type'])
+        Utils.log('AUC: ' + str(accuracy * 100) + '%')
 
         # Save the model
         if epoch % save_epoch == 0 or epoch == config['epochs'] - 1:
@@ -162,9 +170,9 @@ def train(config):
             max_accuracy = accuracy
             torch.save(model, os.path.join(save_path, 'max-va-model.pth'))
 
-        utils.log('----------------------------------------')
+        Utils.log('----------------------------------------')
 
-    utils.log('Training is finished')
+    Utils.log('Training is finished')
 
 
 if __name__ == '__main__':
@@ -180,5 +188,5 @@ if __name__ == '__main__':
         config['_parallel'] = True
         config['_gpu'] = args.gpu
 
-    utils.set_gpu(args.gpu)
+    Utils.set_gpu(args.gpu)
     train(config)
