708ec33721ed58e9c7873b9e8f1c42b97169107d
feihu <565807094@qq.com>
2021-02-18 11:58:24 +0800

    lyx_objectvad

--- readmat.py
+++
@@ -1,9 +0,0 @@
-import scipy.io as scio
-
-# dataFile = 'avenue.mat'
-# data = scio.loadmat(dataFile)
-# print(data['gt'][0][0])
-
-dataFile = 'avenue.mat'
-data = scio.loadmat(dataFile)
-print(data)
